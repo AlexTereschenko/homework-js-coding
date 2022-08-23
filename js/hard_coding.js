d4a40c9e7c9949111edf2926f10725a253aac1e8
Alex Tereschenko <Freelancer.ua.1992@gmail.com>
2022-08-23 14:43:17 +0300

    permute solved

--- js/hard_coding.js
+++ js/hard_coding.js
@@ -1,6 +1,44 @@
-// 1. // permute - Return an array of strings, containing every permutation of the given string.
-// permute('')             // []
-// permute('abc')          // ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+1. // permute - Return an array of strings, containing every permutation of the given string.
+permute('')             // []
+permute('abc')          // ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+
+/////////////////////////////////Solving////////////////////////////////////
+console.log('');
+function permute(str) {
+    let result=[];
+    let counterOfRepeats = str.length-1;
+    let indexOfChr = 0;
+
+    if (counterOfRepeats<0) return console.log('All possible permutaging of string "'+str+'" are ['+result+']')
+
+    permuteInner(str, indexOfChr)
+
+    function permuteInner(str, indexOfChr) {
+
+        if (indexOfChr == counterOfRepeats) result.push(str);
+        else {
+           for (let i = indexOfChr; i <= counterOfRepeats; i++) {
+                str = swap(str, indexOfChr, i);
+                permuteInner(str, indexOfChr + 1);
+                str = swap(str, indexOfChr, i);
+            }
+        }
+    }
+
+    function swap(arr, index, newIndex) {
+        let temp;
+        let charArray = arr.split('');
+
+        temp = charArray[index] ;
+        charArray[index] = charArray[newIndex];
+        charArray[newIndex] = temp;
+
+        return (charArray).join('');
+    }
+
+    return console.log('All possible permutaging of string "'+str+'" are ['+result+']')
+}
+////////////////////////////////////////////////////////////////////////////
 
 // 2. // seq - Resolve an array of promises in sequence (as opposed to Promise.all, which does it in parallel).
 // let a = Promise.resolve('a')
@@ -8,3 +46,6 @@
 // let c = Promise.resolve('c')
 // await seq([a, b, c])                  // ['a', 'b', 'c']
 // await seq([a, c, b])                  // ['a', 'c', 'b']
+
+
+
